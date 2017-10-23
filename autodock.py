1ea954610c9f608236c260fcc38ae397b58b30ca
Carlos Familia <carlosfamilia@gmail.com>
2017-10-23 21:21:17 +0100

    Update to support Pymol 2.0

--- autodock.py
+++ autodock.py
@@ -41,7 +41,7 @@ import tkMessageBox, tkFileDialog
 import Pmw
 from threading import Thread
 #from commands import getstatusoutput
-from pymol import cmd,selector
+from pymol import cmd,selector,plugins
 from pymol.cmd import _feedback,fb_module,fb_mask,is_list,_cmd
 from pymol.cgo import *
 from pymol import stored
@@ -117,7 +117,8 @@ def __init__(self):
 #
 #     SOME BASIC THINGIES
 #
-
+app = plugins.get_pmgapp()
+root = plugins.get_tk_root()
 global_status = StringVar()
 global_status.set("")
 transfer_status = {}
